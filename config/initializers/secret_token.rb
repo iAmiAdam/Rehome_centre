dd289b03d313b330a7a4fec8b26cffe7944443d4
Adam Smith <adam.smith18@ntlworld.com>
2014-10-01 20:36:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RehomeCentre::Application.config.secret_key_base = 'd80da4518226d7a816aaba1b9e96b96d0f100c4ded480785a9a9be9bf3368437b26f03bd811ebd168fa401237697dcb6b356092f890aefcfcd1b15229f9e0be1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RehomeCentre::Application.config.session_store :cookie_store, key: '_rehome_centre_session'
