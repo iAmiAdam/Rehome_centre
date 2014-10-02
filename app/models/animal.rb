ba89b2c6bfe45aa0f40d00e0db4774149332c21a
Adam Smith <adam.smith18@ntlworld.com>
2014-10-02 22:58:02 +0100

    Updated animal migration

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,13 +2,14 @@ class Animal < ActiveRecord::Base
 	# Animals belong to a rescue
 	belongs_to :user
 
-	# Animals must have a name, breed, colour, age, gender and description
+	# Animals must have a name, breed, colour, age, gender, type and description
 	validates :name, presence: true
 	validates :breed, presence: true
 	validates :colour, presence: true
 	validates :age, presence: true
 	validates :description, presence: true
 	validates :gender, presence: true
+	validtes :type, presence: true
 
 	# We need to know if they're speyed or neutered and whether they're vaccinated
 	validates :vaccinate, presence: true
--- db/migrate/20141001194536_create_animals.rb
+++ db/migrate/20141001194536_create_animals.rb
@@ -3,8 +3,15 @@ class CreateAnimals < ActiveRecord::Migration
     create_table :animals do |t|
       t.string :name
       t.string :user_id
+      t.string :description
+      t.integer :colour
+      t.integer :breed
+      t.integer :age
       t.integer :type
-      
+      t.integer :gender
+      t.boolean :vaccinated
+      t.boolean :neutered
+
       t.timestamps
     end
   end
