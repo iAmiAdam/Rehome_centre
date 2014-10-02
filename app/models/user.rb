69a53ec46fa77989db6ef2a12809bd8b3de0cb32
Adam Smith <adam.smith18@ntlworld.com>
2014-10-02 23:10:59 +0100

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	# Hopefully rescues have a lot of animals to rehome
+	has_many :animals
+
+	# We need to make sure users are inputting their data right
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :password_digest, presence: true
+	validates :type, presence: true
 end
