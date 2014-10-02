fa2db36285bbb50417be0239e93e935b791e3c38
Adam Smith <adam.smith18@ntlworld.com>
2014-10-02 22:53:37 +0100

    Animal model updated

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,17 @@
 class Animal < ActiveRecord::Base
+	# Animals belong to a rescue
+	belongs_to :user
+
+	# Animals must have a name, breed, colour, age, gender and description
+	validates :name, presence: true
+	validates :breed, presence: true
+	validates :colour, presence: true
+	validates :age, presence: true
+	validates :description, presence: true
+	validates :gender, presence: true
+
+	# We need to know if they're speyed or neutered and whether they're vaccinated
+	validates :vaccinate, presence: true
+	validates :neutered, presence: true
+
 end
