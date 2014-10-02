4ac2112b29e64a66b95d1401607c3c9593a5db8b
Adam Smith <adam.smith18@ntlworld.com>
2014-10-02 22:47:46 +0100

    Animals migration

--- db/migrate/20141001194536_create_animals.rb
+++ db/migrate/20141001194536_create_animals.rb
@@ -1,7 +1,10 @@
 class CreateAnimals < ActiveRecord::Migration
   def change
     create_table :animals do |t|
-
+      t.string :name
+      t.string :user_id
+      t.integer :type
+      
       t.timestamps
     end
   end
