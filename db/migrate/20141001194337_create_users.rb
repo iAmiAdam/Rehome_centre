30a15dc8117ceb1e400c6621279b1c50735f4f03
Adam Smith <adam.smith18@ntlworld.com>
2014-10-04 21:57:18 +0100

    Updated Users migration

--- db/migrate/20141001194337_create_users.rb
+++ db/migrate/20141001194337_create_users.rb
@@ -2,11 +2,12 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :email, unique: true
       t.string :password_digest
       t.integer :type
       t.integer :number
       t.string :location
+      t.string :remember_token, unique: true
       
       t.timestamps
     end
